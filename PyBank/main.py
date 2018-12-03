57bd75a256a9c2085a7336d9e22b40c08d0b8a55
Grace Hsu <g.liu135@gmail.com>
2018-12-02 19:36:43 -0800

    second update. total months, total profit, greatest inc, and greatest dec calculations work.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,50 @@
 import os
 import csv
 
-bank_path = os.path.join('..', 'Resources', 'budget_data.csv')
+bank_path = os.path.join('Resources', 'budget_data.csv')
 
-with open(bank_path, 'r+', newline='') as bank_path_file:
-    bank_path_file = csv.reader(bank_path_file, delimiter=',')
+#create a file for the financial summary
+##financial_analysis_summary = open('financial_analysis.txt', 'a').close()
     
-    financial_analysis_summary = open("financial_analysis.txt", "w")
+with open(bank_path, 'r', newline='') as bank_file:
+    bank_file = csv.reader(bank_file, delimiter=',')
     
-    file_header = Next(bank_path_file)
+    file_header = next(bank_file)
+    
+    monthly_data = []
+    
+    for row in bank_file:
+        monthly_data.append(row)
+    
+    total_months = len(monthly_data)
+    net_profit = 0
+    average_change = 0.00
+    greatest_inc = 0 
+    greatest_dec = 0
     
     
+    
+    for each in monthly_data:
+        current_profit = int(each[1])
+        if current_profit > int(greatest_inc):
+            greatest_inc_month = each[0]
+            greatest_inc = current_profit
+        elif current_profit < int(greatest_dec):
+            greatest_dec_month = each[0]
+            greatest_dec = current_profit
+        
+    for each in monthly_data:
+        net_profit += int(each[1])
+        
     
     print("Financial Analysis")
     print("----------------------------------------------------------")
+    print(f'Total Months: {total_months}')
+    print(f'Total: $ {net_profit}')
+    print(f'Average change: $ {average_change}')
+    print(f'Greatest Increase in Profits: {greatest_inc_month} (${greatest_inc})')
+    print(f'Greatest Decrease in Profits: {greatest_dec_month} (${greatest_dec})')
     
     
     
-    #is this really needed? I read some forums saying that the 'with open' syntax automatically closes the opened file for you.
-    bank_path_file.close()
+    
